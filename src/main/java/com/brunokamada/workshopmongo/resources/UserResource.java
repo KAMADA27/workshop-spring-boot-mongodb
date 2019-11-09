4dfffa072436197d8787379466b9112911de25b4
user_arpia27 <brunokamada72@gmail.com>
2019-11-09 14:02:33 -0300

    create UserDTO for return users

--- src/main/java/com/brunokamada/workshopmongo/resources/UserResource.java
+++ src/main/java/com/brunokamada/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.brunokamada.workshopmongo.resources;
 
 import com.brunokamada.workshopmongo.domain.User;
+import com.brunokamada.workshopmongo.dto.UserDTO;
 import com.brunokamada.workshopmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,9 +22,10 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 
-        return ResponseEntity.ok().body(list);
+        return ResponseEntity.ok().body(listDto);
     }
 }
